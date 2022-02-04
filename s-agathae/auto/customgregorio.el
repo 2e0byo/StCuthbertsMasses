5dcdb4726ee996b5d8a74bc28d74b0f86fb2894d
John Maximilian <2e0byo@gmail.com>
2022-02-04 21:47:55 +0000

    chore: didn't mean to commit .els.

--- s-agathae/auto/customgregorio.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "customgregorio"
- (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("gregoriotex" "nevercompile")))
-   (TeX-run-style-hooks
-    "gregoriotex"))
- :latex)
-
